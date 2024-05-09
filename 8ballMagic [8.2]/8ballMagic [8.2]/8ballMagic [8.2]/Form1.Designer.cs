5175573a06cd27c76ee91f12666a498ecc31fc76
qwerroaaa <serbushko.qwe@gmail.com>
2024-05-09 17:47:30 +0700

    added 8.2
    in summary

---
+++ 8ballMagic [8.2]/8ballMagic [8.2]/8ballMagic [8.2]/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace _8ballMagic__8._2_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.predictBtn = new System.Windows.Forms.Button();
+            this.AnswerLabel = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Image = global::_8ballMagic__8._2_.Properties.Resources.Magic_eight_ball;
+            this.pictureBox1.Location = new System.Drawing.Point(136, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 500);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // predictBtn
+            // 
+            this.predictBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.predictBtn.Location = new System.Drawing.Point(367, 551);
+            this.predictBtn.Name = "predictBtn";
+            this.predictBtn.Size = new System.Drawing.Size(147, 41);
+            this.predictBtn.TabIndex = 1;
+            this.predictBtn.Text = "Узнать ответ";
+            this.predictBtn.UseVisualStyleBackColor = true;
+            this.predictBtn.Click += new System.EventHandler(this.predictBtn_Click);
+            // 
+            // AnswerLabel
+            // 
+            this.AnswerLabel.AutoSize = true;
+            this.AnswerLabel.BackColor = System.Drawing.Color.Blue;
+            this.AnswerLabel.ForeColor = System.Drawing.Color.White;
+            this.AnswerLabel.Location = new System.Drawing.Point(415, 232);
+            this.AnswerLabel.Name = "AnswerLabel";
+            this.AnswerLabel.Size = new System.Drawing.Size(0, 13);
+            this.AnswerLabel.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(315, 641);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(256, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.label2.Location = new System.Drawing.Point(341, 612);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(191, 26);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Выберите вопрос";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 737);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.AnswerLabel);
+            this.Controls.Add(this.predictBtn);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button predictBtn;
+        private System.Windows.Forms.Label AnswerLabel;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 8ballMagic [8.2]/8ballMagic [8.2]/8ballMagic [8.2]/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8ballMagic__8._2_
+{
+    public partial class Form1 : Form
+    {
+        List<KeyValuePair<string, double>> pairs = new List<KeyValuePair<string, double>>()
+            {
+                new KeyValuePair<string, double>("Определенно да!", 0.2),
+                new KeyValuePair<string, double>("Однозначно", 0.1),
+                new KeyValuePair<string, double>("Да", 0.15),
+                new KeyValuePair<string, double>("Не определено", 0.05),
+                new KeyValuePair<string, double>("Вероятно", 0.1),
+                new KeyValuePair<string, double>("Нет", 0.15),
+                new KeyValuePair<string, double>("Неоднозначно", 0.15),
+                new KeyValuePair<string, double>("Абсолютно нет!", 0.1)
+            };
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+
+            comboBox1.Items.AddRange(new string[] {
+                "Пойти ли завтра на пару?",
+                "Будет ли завтра дождь?",
+                "Сделать ли лабораторную?",
+                "Подешевеет ли гречка?",
+                "Будет ли завтра выходной?",
+                "Существует ли Бог?"
+            });
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void predictBtn_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            double prob = random.NextDouble();
+
+            foreach (var pair in pairs)
+            {
+                prob -= pair.Value;
+                if (prob <= 0)
+                {
+                    AnswerLabel.Text = pair.Key;
+                    break;
+                }
+            }
+        }
+    }
+}
