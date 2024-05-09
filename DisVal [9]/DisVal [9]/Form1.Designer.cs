9b3ba2a25a5b56aa3880796849419f0d21370a96
qwerroaaa <serbushko.qwe@gmail.com>
2024-05-09 21:55:17 +0700

    added 9
    in summary

---
+++ DisVal [9]/DisVal [9]/Form1.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace DisVal__9_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.meanLabel = new System.Windows.Forms.Label();
+            this.varLabel = new System.Windows.Forms.Label();
+            this.ChiLabel = new System.Windows.Forms.Label();
+            this.relErrorLabel = new System.Windows.Forms.Label();
+            this.VarErrorLabel = new System.Windows.Forms.Label();
+            this.otvergaem = new System.Windows.Forms.Label();
+            this.prinimaem = new System.Windows.Forms.Label();
+            this.fromfunc = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Вероятность 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Вероятность 2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Вероятность 3:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Вероятность 4:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(48, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Вероятность 5:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(48, 194);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Число попыток:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(138, 191);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(138, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "0,1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(138, 68);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "0,3";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(138, 97);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(121, 20);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "0,3";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(138, 128);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(121, 20);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.Text = "0,2";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(138, 157);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(121, 20);
+            this.textBox5.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(48, 20);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(148, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Введите ваши вероятности:";
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(294, 19);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(484, 355);
+            this.chart1.TabIndex = 13;
+            this.chart1.Text = "chart1";
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(96, 259);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(99, 40);
+            this.StartButton.TabIndex = 14;
+            this.StartButton.Text = "Начать";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(307, 377);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(50, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Average:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(307, 401);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(52, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Variance:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(307, 428);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(66, 13);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Chi-squared:";
+            // 
+            // meanLabel
+            // 
+            this.meanLabel.AutoSize = true;
+            this.meanLabel.Location = new System.Drawing.Point(382, 377);
+            this.meanLabel.Name = "meanLabel";
+            this.meanLabel.Size = new System.Drawing.Size(0, 13);
+            this.meanLabel.TabIndex = 18;
+            // 
+            // varLabel
+            // 
+            this.varLabel.AutoSize = true;
+            this.varLabel.Location = new System.Drawing.Point(382, 401);
+            this.varLabel.Name = "varLabel";
+            this.varLabel.Size = new System.Drawing.Size(0, 13);
+            this.varLabel.TabIndex = 19;
+            // 
+            // ChiLabel
+            // 
+            this.ChiLabel.AutoSize = true;
+            this.ChiLabel.Location = new System.Drawing.Point(382, 428);
+            this.ChiLabel.Name = "ChiLabel";
+            this.ChiLabel.Size = new System.Drawing.Size(0, 13);
+            this.ChiLabel.TabIndex = 20;
+            // 
+            // relErrorLabel
+            // 
+            this.relErrorLabel.AutoSize = true;
+            this.relErrorLabel.Location = new System.Drawing.Point(476, 377);
+            this.relErrorLabel.Name = "relErrorLabel";
+            this.relErrorLabel.Size = new System.Drawing.Size(0, 13);
+            this.relErrorLabel.TabIndex = 21;
+            // 
+            // VarErrorLabel
+            // 
+            this.VarErrorLabel.AutoSize = true;
+            this.VarErrorLabel.Location = new System.Drawing.Point(476, 401);
+            this.VarErrorLabel.Name = "VarErrorLabel";
+            this.VarErrorLabel.Size = new System.Drawing.Size(0, 13);
+            this.VarErrorLabel.TabIndex = 22;
+            // 
+            // otvergaem
+            // 
+            this.otvergaem.AutoSize = true;
+            this.otvergaem.Location = new System.Drawing.Point(625, 428);
+            this.otvergaem.Name = "otvergaem";
+            this.otvergaem.Size = new System.Drawing.Size(0, 13);
+            this.otvergaem.TabIndex = 23;
+            // 
+            // prinimaem
+            // 
+            this.prinimaem.AutoSize = true;
+            this.prinimaem.Location = new System.Drawing.Point(631, 428);
+            this.prinimaem.Name = "prinimaem";
+            this.prinimaem.Size = new System.Drawing.Size(0, 13);
+            this.prinimaem.TabIndex = 24;
+            // 
+            // fromfunc
+            // 
+            this.fromfunc.AutoSize = true;
+            this.fromfunc.Location = new System.Drawing.Point(509, 428);
+            this.fromfunc.Name = "fromfunc";
+            this.fromfunc.Size = new System.Drawing.Size(0, 13);
+            this.fromfunc.TabIndex = 25;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.fromfunc);
+            this.Controls.Add(this.prinimaem);
+            this.Controls.Add(this.otvergaem);
+            this.Controls.Add(this.VarErrorLabel);
+            this.Controls.Add(this.relErrorLabel);
+            this.Controls.Add(this.ChiLabel);
+            this.Controls.Add(this.varLabel);
+            this.Controls.Add(this.meanLabel);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label meanLabel;
+        private System.Windows.Forms.Label varLabel;
+        private System.Windows.Forms.Label ChiLabel;
+        private System.Windows.Forms.Label relErrorLabel;
+        private System.Windows.Forms.Label VarErrorLabel;
+        private System.Windows.Forms.Label otvergaem;
+        private System.Windows.Forms.Label prinimaem;
+        private System.Windows.Forms.Label fromfunc;
+    }
+}
+
---
+++ DisVal [9]/DisVal [9]/Form1.cs
@@ -0,0 +1,107 @@
+﻿using MathNet.Numerics.Distributions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace DisVal__9_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.AddRange(new string[] {
+                "10",
+                "100",
+                "1000",
+                "10000"
+            });
+
+            comboBox1.SelectedIndex = 0;
+        }
+        private Random rand = new Random();
+        private double[] probArray = new double[5];
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            probArray[0] = double.Parse(textBox1.Text);
+            probArray[1] = double.Parse(textBox2.Text);
+            probArray[2] = double.Parse(textBox3.Text);
+            probArray[3] = double.Parse(textBox4.Text);
+            probArray[4] = 1 - (probArray[0] + probArray[1] + probArray[2] + probArray[3]);
+            if (probArray[4] < 0)
+            {
+                MessageBox.Show("Сумма ваших вероятностей превышает 1");
+                return;
+            }
+            textBox5.Text = probArray[4].ToString();
+
+            int N = int.Parse(comboBox1.Text);
+            int[] statistics = new int[probArray.Length];
+
+            for (int i = 0; i < N; i++)
+            {
+                double randNum = rand.NextDouble();
+                for (int j = 0; j < probArray.Length; j++)
+                {
+                    randNum -= probArray[j];
+                    if (randNum <= 0)
+                    {
+                        statistics[j]++;
+                        break;
+                    }
+                }
+            }
+            chart1.Series[0].Points.Clear();
+            for (int i = 0; i < probArray.Length; i++)
+            {
+                chart1.Series[0].Points.AddXY($"Событие {i + 1}", (double)statistics[i] / N);
+            }
+            double average = 0;
+            double variance = 0;
+            double ChiSquare = 0;
+            for (int i = 0; i < probArray.Length; i++)
+            {
+                average += i * ((double)statistics[i] / N);
+                variance += i * i * ((double)statistics[i] / N);
+                if (statistics[i] > 0) 
+                {
+                    ChiSquare += (statistics[i] - N * probArray[i]) * (statistics[i] - N * probArray[i]) / (N * probArray[i]);
+                }
+            }
+            variance -= average * average;
+
+            meanLabel.Text = average.ToString();
+            varLabel.Text = variance.ToString();
+            ChiLabel.Text = ChiSquare.ToString();
+
+            double relError = Math.Abs((average - probArray.Sum()) / probArray.Sum());
+            relErrorLabel.Text = relError.ToString();
+            double varError =Math.Abs((variance - probArray.Sum(p => p * p)) / probArray.Sum(p => p * p));
+            VarErrorLabel.Text = varError.ToString();
+
+            double alpha = 0.05;
+            int df = probArray.Length - 1;
+            double takeChiSquareValue = ChiSquared.InvCDF(df, 1 - alpha);
+            fromfunc.Text = takeChiSquareValue.ToString();
+            if (ChiSquare > takeChiSquareValue)
+            {
+                otvergaem.Text = "Отвергаем";
+                otvergaem.Visible = true;
+                prinimaem.Visible = false;
+            } 
+            else
+            {
+                prinimaem.Text = "Не отвергается";
+                prinimaem.Visible = true;
+                otvergaem.Visible = false;
+            }
+        }
+    }
+}
